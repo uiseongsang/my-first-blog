4582f6ec51cea45cc5fb21cff798ab5c40606748
thomas <dmltjd102@gmail.com>
2018-07-22 04:10:28 -0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
+             #identify the pk in post_list.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+   
+    # For render function, has request and templets
+    # To use the templets, must add {'posts': posts}
     return render(request, 'blog/post_list.html', {'posts': posts})
 	
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
